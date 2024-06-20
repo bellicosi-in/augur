db0ad42330720d6fe8c6100edbd1310a86a8d0a3
bellicosi-in <73630139+bellicosi-in@users.noreply.github.com>
2024-06-20 16:37:04 +0530

    its training like shit

--- generate_dataset.py
+++ generate_dataset.py
@@ -1,10 +1,12 @@
 import os
 import chess.pgn 
+import numpy as np
 from state import State
 
 def get_dataset(num_samples = None):
     X,Y = [],[]
     gn = 0
+    values = {'1/2-1/2':0, '0-1':-1, '1-0':1}
     for fn in os.listdir("data"):
         pgn = open(os.path.join("data",fn))
         while 1:
@@ -13,10 +15,12 @@ def get_dataset(num_samples = None):
             except Exception:
                 break
             
-            gn += 1
-            result = {"1/2-1/2" : 0, "1-0" : 1, "0-1" : -1}[game.headers['Result']]
+            # gn += 1
+            result = game.headers['Result']
             # print(result)
-
+            if result not in values:
+                continue
+            value = values[result]
             board = game.board()
             for i,move in enumerate(game.mainline_moves()):
                 # print(i)
@@ -25,11 +29,18 @@ def get_dataset(num_samples = None):
                 # print(ser)
                 # print(board.shredder_fen())
                 X.append(ser)
-                Y.append(result)
+                Y.append(value)
             print(f"parsing {gn} games and got {len(X)} samples")
             if num_samples is not None and len(X)>num_samples:
                 return X,Y
-            
+            gn += 1
+        X = np.array(X)
+        Y = np.array(Y)
+        return X,Y 
 
 if __name__ == "__main__":
-    X,Y = get_dataset(1000)
+    X,Y = get_dataset(100000)
+
+    np.savez("processed/dataset_100k.npz",X,Y) 
+    
+    print("file saved")
--- train.py
+++ train.py
@@ -1,21 +1,103 @@
-import os 
-import chess.pgn
-from state import State
+import torch
+import torch.nn as nn
+import numpy as np
+import torch.nn.functional as F
+from torch.utils.data import Dataset
+from torch import optim
 
-for fn in os.listdir("data"):
-    pgn = open(os.path.join("data",fn))
-    while 1:
-        try:
-            game = chess.pgn.read_game(pgn)
-        except Exception:
-            break
-        value = {'1/2-1/2':0, '0-1' :-1, '1-0':1}[game.headers['Result']]
-        # print(value)
-        # result = game.headers['Result']
-        board = game.board()
-        for i,move in enumerate(game.mainline_moves()):
-            board.push(move)
-            print(value,shredder_fen_to_vec(board.shredder_fen()))
-            # TODO: exract the boardx
-        exit(0)
+class ChessDataset(Dataset):
+    def __init__(self):
+        dat = np.load("processed/dataset_100k.npz")
+        self.X = dat['arr_0']
+        self.Y = dat['arr_1']
+        self.X = self.X.reshape(-1, 5, 4, 2)
+
+         
+        print("loaded", self.X.shape,self.Y.shape)
     
+    def __len__(self):
+        return self.X.shape[0]
+
+    def __getitem__(self,idx):
+        return (self.X[idx], self.Y[idx])
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net,self).__init__()
+        self.a1 = nn.Conv2d(5,16,kernel_size = 3, padding = 1)
+        self.a2 = nn.Conv2d(16,16, kernel_size =  3, padding = 1)
+        self.a3 = nn.Conv2d(16,32, kernel_size =  3, padding = 1)
+
+        self.b1 = nn.Conv2d(32,32, kernel_size =  3, padding = 1)
+        self.b2 = nn.Conv2d(32,64, kernel_size =  3, padding = 1)
+        self.b3 = nn.Conv2d(64,128, kernel_size =  3, padding = 1)
+
+        self.c1 = nn.Conv2d(128, 128, kernel_size=1)
+        self.c2 = nn.Conv2d(128, 128, kernel_size=1)
+        self.c3 = nn.Conv2d(128, 128, kernel_size=1)
+
+        
+        self.last = nn.Linear(128,1)
+    
+    def forward(self,x):
+        # x = x.permute(0,3,1,2)
+        x = F.relu(self.a1(x))
+        x = F.relu(self.a2(x))
+        x = F.relu(self.a3(x))
+        # x = F.max_pool2d(x,2)
+
+        # 4x4
+        x = F.relu(self.b1(x))
+        x = F.relu(self.b2(x))
+        x = F.relu(self.b3(x))
+        # x = F.max_pool2d(x, 2)i
+
+        x = F.relu(self.c1(x))
+        x = F.relu(self.c2(x))
+        x = F.relu(self.c3(x))
+
+        # Add an adaptive pooling layer to adjust the feature map size to (1, 1)
+        x = F.adaptive_avg_pool2d(x, (1, 1))
+        # print("Shape after adaptive pooling:", x.shape)  # Debugging statement
+
+        x = x.view(x.size(0), -1)  # Flatten the output
+        # print("Shape after view:", x.shape)  # Debugging statement
+
+        x = self.last(x)
+
+        return F.tanh(x)
+    
+
+chess_dataset = ChessDataset()
+print(chess_dataset.X)
+train_loader = torch.utils.data.DataLoader(chess_dataset, batch_size = 256, shuffle = True)
+model = Net()
+optimizer = optim.Adam(model.parameters(),lr = 0.1)
+floss = nn.MSELoss()
+
+device = (
+     "cpu"
+)
+
+model.train()
+
+for epoch in range(100):
+    all_loss = 0
+    for batch_idx, (data,target) in enumerate(train_loader):
+        target = target.unsqueeze(-1)
+        data, target = data.to(device), target.to(device)
+        data = data.float()
+        # data = data.reshape((1,256,5,8))
+        target = target.float()
+        
+        optimizer.zero_grad()
+        output = model(data)
+        
+        loss = floss(output,target)
+        loss.backward()
+        optimizer.step()
+
+        all_loss += loss.item()
+    print(f"{epoch}, {all_loss}")
+
