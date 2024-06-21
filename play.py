32d808010151baea8ee05b858a9c358e7e393f36
bellicosi-in <73630139+bellicosi-in@users.noreply.github.com>
2024-06-21 13:27:56 +0530

    finally playing the shit

--- play.py
+++ play.py
@@ -3,6 +3,7 @@ import torch
 from train import Net
 import chess.svg
 import time
+import traceback
 
 
 class Valuator(object):
@@ -38,7 +39,7 @@ def hello():
     ret += '<style>input{ font-size: 30px;} button {font-size: 30px;}</style>'
     ret += '</head><body>'
     ret += '<img width = 600 src = "/board.svg?%f"></img><br/>' %time.time()
-    ret += 'form action = "/move"><input name = "move" type = "text"></input><input type = "submit" value = "Move"></form><br/>'
+    ret += '<form action = "/move"><input name = "move" type = "text"></input><input type = "submit" value = "Move"></form><br/>'
     return ret
 
 @app.route("/board.svg")
@@ -48,16 +49,19 @@ def board():
 def computer_move():
     move = sorted(explore_leaves(s,v), key = lambda x:x[0], reverse = s.board.turn)[0]
     print(move)
-    s.board.push(move)
+    s.board.push(move[1])
 
 @app.route("/move")
 def move():
     if not s.board.is_game_over():
-        move = request.args.get('move',defaults = "")
+        move = request.args.get('move',default = "")
         if move is not None and move!= "":
             print("human moves",move)
-            s.board.push_san(move)
-            computer_move()
+            try:
+                s.board.push_san(move)
+                computer_move()
+            except Exception:
+                traceback.print_exc()
         else:
             print("Game is over!")
         return hello()
@@ -74,7 +78,7 @@ if __name__ == "__main__":
 #     # self play
 #     while not s.board.is_game_over():
 #         l = sorted(explore_leaves(s,v), key = lambda x: x[0], reverse = s.board.turn)
-#         move = l[0]
+#         move = l[0]``
 #         print(move)
 #         s.board.push(move[1])
 #     print(s.board.result())
