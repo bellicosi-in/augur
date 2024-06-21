e84f6b609dd061d7a9820ffcbb7491389dabc516
bellicosi-in <73630139+bellicosi-in@users.noreply.github.com>
2024-06-21 13:14:17 +0530

    basic chess gui without any support for play

--- play.py
+++ play.py
@@ -1,6 +1,9 @@
 from state import State
 import torch
 from train import Net
+import chess.svg
+import time
+
 
 class Valuator(object):
     def __init__(self):
@@ -14,13 +17,64 @@ class Valuator(object):
         return float(output.data[0][0])
     
 
-
-
-if __name__ == "__main__":
-    v = Valuator()
-    s = State()
-    print(v(s))
+def explore_leaves(s,v):
+    ret = []
     for e in s.edges():
         s.board.push(e)
-        print(e,v(s))
+        ret.append((v(s),e))
         s.board.pop()
+    return ret
+
+v = Valuator()
+s = State()
+
+from flask import Flask,Response,request
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    ret = '<html><head>'
+    ret += '<style>input{ font-size: 30px;} button {font-size: 30px;}</style>'
+    ret += '</head><body>'
+    ret += '<img width = 600 src = "/board.svg?%f"></img><br/>' %time.time()
+    ret += 'form action = "/move"><input name = "move" type = "text"></input><input type = "submit" value = "Move"></form><br/>'
+    return ret
+
+@app.route("/board.svg")
+def board():
+    return Response(chess.svg.board(board = s.board), mimetype = 'image/svg+xml')
+
+def computer_move():
+    move = sorted(explore_leaves(s,v), key = lambda x:x[0], reverse = s.board.turn)[0]
+    print(move)
+    s.board.push(move)
+
+@app.route("/move")
+def move():
+    if not s.board.is_game_over():
+        move = request.args.get('move',defaults = "")
+        if move is not None and move!= "":
+            print("human moves",move)
+            s.board.push_san(move)
+            computer_move()
+        else:
+            print("Game is over!")
+        return hello()
+
+if __name__ == "__main__":
+    app.run(debug = True)
+
+
+
+
+# if __name__ == "__main__":
+    
+
+#     # self play
+#     while not s.board.is_game_over():
+#         l = sorted(explore_leaves(s,v), key = lambda x: x[0], reverse = s.board.turn)
+#         move = l[0]
+#         print(move)
+#         s.board.push(move[1])
+#     print(s.board.result())
